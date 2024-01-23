4bbb9d19d641067ce93496e84b8c0c6d09b8e901
Sharada Marasinha <86095574+sharada-marasinha@users.noreply.github.com>
2024-01-23 21:05:35 +0530

    get API done

--- src/main/java/org/example/repository/BookRepository.java
+++ src/main/java/org/example/repository/BookRepository.java
@@ -1,7 +1,8 @@
 package org.example.repository;
 
 import org.example.entity.BookEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BookRepository extends CrudRepository<BookEntity,Long> {
+public interface BookRepository extends JpaRepository<BookEntity,Long> {
 }
--- src/main/java/org/example/service/impl/BookServiceImpl.java
+++ src/main/java/org/example/service/impl/BookServiceImpl.java
@@ -1,5 +1,6 @@
 package org.example.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.example.dto.Book;
 import org.example.entity.BookEntity;
 import org.example.repository.BookRepository;
@@ -9,11 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
-@Service
-public class BookServiceImpl implements BookService {
-    @Autowired
-    BookRepository repository;
+import java.util.List;
 
+@Service
+@RequiredArgsConstructor
+public class BookServiceImpl implements BookService {
+
+   final BookRepository repository;
     ModelMapper mapper;
     @Bean
     public void setup(){
@@ -23,5 +26,10 @@ public class BookServiceImpl implements BookService {
     public void addBook(Book book) {
         BookEntity entity = mapper.map(book, BookEntity.class);
         repository.save(entity);
+    }
+
+    @Override
+    public List<BookEntity> getBooks() {
+        return repository.findAll();
     }
 }
